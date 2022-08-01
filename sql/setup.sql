23698243c28522c590f83aefe63d4a3ba416f7e7
Allison Ause <allison.ause@gmail.com>
2022-08-01 16:42:02 -0700

    seeded data for authors table

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,12 +4,13 @@
 -- TABLES = Books, Authors, books_authors
 
 DROP table if exists books;
+Drop table if exists authors;
 
 CREATE table books (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title VARCHAR NOT NULL,
   released INT NOT NULL
-)
+);
 
 INSERT INTO books (title, released) VALUES
 ('Gods War', 2004),
@@ -18,3 +19,15 @@ INSERT INTO books (title, released) VALUES
 ('The Comfort Book', 2022),
 ('The Love Hypothesis', 2021);
 
+
+CREATE table authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  dob DATE,
+  pod VARCHAR
+);
+
+INSERT INTO authors (name, dob, pod) VALUES
+('Katherine Hurley', '1986-05-05', 'Florida'), 
+('Matt Haig', '1957-06-06', 'England'),
+('Ali Hazelwood', '1998-07-07', 'Chicago');
