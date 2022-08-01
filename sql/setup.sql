f2a7db2a3ff158406d3cb24f4af8c1dd0011a557
Allison Ause <allison.ause@gmail.com>
2022-08-01 16:28:04 -0700

    Seeded data for books table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+-- TABLES = Books, Authors, books_authors
+
+DROP table if exists books;
+
+CREATE table books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+)
+
+INSERT INTO books (title, released) VALUES
+('Gods War', 2004),
+('The Stars Are Legion', 2018),
+('The Midnight Library', 2020),
+('The Comfort Book', 2022),
+('The Love Hypothesis', 2021);
+
