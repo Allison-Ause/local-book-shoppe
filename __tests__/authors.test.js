31c2676a297e1387f90142e25bb7b24ea67ef15a
Allison Ause <allison.ause@gmail.com>
2022-08-02 16:23:49 -0700

    Confirmed test passing for getbyId for authors

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -11,15 +11,17 @@ describe('author routes', () => {
     const res = await request(app).get('/authors');
     expect(res.body.length === 5);
   });
-  it('/books/:id returns title, released and nested authors', async () => {
-    const res = await request(app).get('/authors/6');
+  it('/authors/:id returns title, released and nested authors', async () => {
+    const res = await request(app).get('/authors/1');
+    console.log('res.body is', res.body);
     expect(res.body).toMatchObject({
+      id: '1',
       name: 'Kameron Hurley',
       dob: expect.any(String),
-      pob: 'Florida',
+      pod: 'Florida',
       books: [
         { id: 1, title: 'Gods War', released: 2004 },
-        { id: 2, title: 'Stars Are Legion', released: 2018 },
+        { id: 2, title: 'The Stars Are Legion', released: 2018 },
       ],
     });
   });
